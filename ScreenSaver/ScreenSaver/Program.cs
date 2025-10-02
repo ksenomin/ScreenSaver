c045d3270f6c64b85575846c8b55d84642b5e983
ksenomin <ksenomin@mail.ru>
2025-10-03 00:50:54 +0300

    Заполнение класса снежинки

---
+++ ScreenSaver/ScreenSaver/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ScreenSaver
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
+}
